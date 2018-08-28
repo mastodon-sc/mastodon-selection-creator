2733f3320ba0d6623a146e25e611d470e6e77aad
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2018-08-28 18:27:48 +0200

    Vertex and edge iterators for SelectionVariable.

--- src/main/java/org/mastodon/revised/ui/selection/creator/evaluation/SelectionVariable.java
+++ src/main/java/org/mastodon/revised/ui/selection/creator/evaluation/SelectionVariable.java
@@ -1,11 +1,14 @@
 package org.mastodon.revised.ui.selection.creator.evaluation;
 
 import java.util.BitSet;
+import java.util.Iterator;
 
+import org.mastodon.RefPool;
 import org.mastodon.graph.Edge;
 import org.mastodon.graph.GraphIdBimap;
 import org.mastodon.graph.Vertex;
 import org.mastodon.model.SelectionModel;
+import org.mastodon.revised.ui.selection.creator.evaluation.util.BitSetIterator;
 
 /**
  * We have to use 4 BitSets, because we want to track also the IDs of the
@@ -101,5 +104,63 @@ public class SelectionVariable
 	public void clearEdges()
 	{
 		selectedEdges.clear();
+	}
+
+	/**
+	 * Returns an iterator that will iterate over the vertices that are set in
+	 * this {@link SelectionVariable}.
+	 *
+	 * @param pool
+	 *            the vertex pool in which to take objects.
+	 * @return a new iterator.
+	 */
+	public < V > Iterator< V > vertexIterator( final RefPool< V > pool )
+	{
+		final V ref = pool.createRef();
+		final BitSetIterator it = new BitSetIterator( selectedVertices );
+		return new Iterator< V >()
+		{
+
+			@Override
+			public boolean hasNext()
+			{
+				return it.hasNext();
+			}
+
+			@Override
+			public V next()
+			{
+				return pool.getObject( it.next(), ref );
+			}
+		};
+	}
+
+	/**
+	 * Returns an iterator that will iterate over the edges that are set in this
+	 * {@link SelectionVariable}.
+	 *
+	 * @param pool
+	 *            the edge pool in which to take objects.
+	 * @return a new iterator.
+	 */
+	public < E > Iterator< E > edgeIterator( final RefPool< E > pool )
+	{
+		final E ref = pool.createRef();
+		final BitSetIterator it = new BitSetIterator( selectedEdges );
+		return new Iterator< E >()
+		{
+
+			@Override
+			public boolean hasNext()
+			{
+				return it.hasNext();
+			}
+
+			@Override
+			public E next()
+			{
+				return pool.getObject( it.next(), ref );
+			}
+		};
 	}
 }
