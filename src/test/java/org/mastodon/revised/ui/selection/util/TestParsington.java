79990f72acdf618ddc395d497522d89f3af18104
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2018-08-29 19:27:11 +0200

    Examples for the selection creator parser.

--- src/test/java/org/mastodon/revised/ui/selection/util/TestParsington.java
+++ src/test/java/org/mastodon/revised/ui/selection/util/TestParsington.java
@@ -1,22 +1,37 @@
 package org.mastodon.revised.ui.selection.util;
 
+import java.awt.Color;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.mastodon.graph.GraphIdBimap;
-import org.mastodon.graph.algorithm.TreeOutputter;
 import org.mastodon.graph.object.ObjectGraph;
 import org.mastodon.graph.object.ObjectVertex;
 import org.mastodon.model.SelectionModel;
 import org.mastodon.revised.mamut.MamutProject;
 import org.mastodon.revised.mamut.MamutProjectIO;
 import org.mastodon.revised.mamut.WindowManager;
+import org.mastodon.revised.mamut.feature.MamutFeatureComputer;
+import org.mastodon.revised.mamut.feature.MamutFeatureComputerService;
+import org.mastodon.revised.mamut.feature.SpotGaussFilteredIntensityComputer;
 import org.mastodon.revised.model.feature.FeatureModel;
 import org.mastodon.revised.model.mamut.Link;
 import org.mastodon.revised.model.mamut.Model;
 import org.mastodon.revised.model.mamut.ModelGraph;
+import org.mastodon.revised.model.mamut.ModelUtils;
 import org.mastodon.revised.model.mamut.Spot;
 import org.mastodon.revised.model.tag.TagSetModel;
+import org.mastodon.revised.model.tag.TagSetStructure;
+import org.mastodon.revised.model.tag.TagSetStructure.Tag;
+import org.mastodon.revised.model.tag.TagSetStructure.TagSet;
+import org.mastodon.revised.ui.ProgressListener;
 import org.mastodon.revised.ui.selection.creator.evaluation.SelectionEvaluator;
+import org.mastodon.revised.ui.selection.creator.evaluation.SelectionVariable;
 import org.scijava.Context;
 import org.scijava.parse.ExpressionParser;
 import org.scijava.parse.SyntaxTree;
@@ -40,20 +55,78 @@ public class TestParsington
 		final TagSetModel< Spot, Link > tagSetModel = model.getTagSetModel();
 		final FeatureModel featureModel = model.getFeatureModel();
 
-		System.out.println( tagSetModel.getTagSetStructure() ); // DEBUG
+		// Create a JY tag.
+		final TagSetStructure tss = new TagSetStructure();
+		tss.createTagSet( "Reviewed by" ).createTag( "JY", Color.GREEN.getRGB() );
+		tagSetModel.setTagSetStructure( tss );
+		// Re-acquire new tag set (because the model stores a copy)
+		final TagSet tagSet2 = tagSetModel.getTagSetStructure().getTagSets().get( 0 );
+		final Tag jyTag = tagSet2.getTags().get( 0 );
 
-		final SyntaxTree tree = new ExpressionParser().parseTree(
+		// Assign JY tag to random vertices and edges.
+		final Random ran = new Random( 1l );
+		for ( final Spot v : graph.vertices() )
+			if ( ran.nextBoolean() )
+				tagSetModel.getVertexTags().set( v, jyTag );
+		for ( final Link e : graph.edges() )
+			if ( ran.nextBoolean() )
+				tagSetModel.getEdgeTags().set( e, jyTag );
+
+		// Compute feature values.
+		final MamutFeatureComputerService service = context.getService( MamutFeatureComputerService.class );
+		final Collection< MamutFeatureComputer > featureComputers = service.getFeatureComputers();
+		final Set< MamutFeatureComputer > fcs = featureComputers.stream()
+				.filter( ( mfc ) -> mfc.getKey() != SpotGaussFilteredIntensityComputer.KEY )
+				.collect( Collectors.toSet() );
+		service.compute( model, featureModel, fcs, voidLogger() );
+
+		final List< String > tests = Arrays.asList( new String[] {
+				"paf > 3",
+
 				"morph( "
-				+ "vertexFeature(SpotPosition, X) > 3. "
-				+ "& "
-						+ "tagSet('Reviewed by') == JY"
-						+ ", (toVertex, toIncomingEdges) )" );
-		final String str = TreeOutputter.output( toMastodon( tree ) );
-		System.out.println( str );
+				+ 		"( vertexFeature('Spot N links') == 3 & vertexFeature('Spot frame') == 14 )"
+				+ ", ('toVertex', 'outgoingEdges') )",
 
-		new SelectionEvaluator<>( graph, graphIdBimap, tagSetModel, featureModel, selectionModel )
-				.evaluate( tree );
+				"vertexFeature('Spot N links') == 3 | vertexFeature('Spot frame') == 25",
+
+				"( vertexFeature('Spot N links') == 3 ) + ( vertexFeature('Spot frame') == 25 )",
+
+				"vertexFeature('Spot N links') == 3 & vertexFeature('Spot frame') == 25",
+
+				"tagSet('Reviewed by') == 'JY'",
+
+				"vertexFeature('Spot position', 'X') > 100. "
+		} );
+
+		final SyntaxTree tree = new ExpressionParser().parseTree( tests.get( 0 ) );
+		final SelectionEvaluator< Spot, Link > evaluator = new SelectionEvaluator<>( graph, graphIdBimap, tagSetModel, featureModel, selectionModel );
+		try
+		{
+			final Object result = evaluator.evaluate( tree );
+			if (result instanceof SelectionVariable)
+			{
+				System.out.println( "Evaluation successful. Content of the selection variable:" );
+				final SelectionVariable sv = ( SelectionVariable ) result;
+				final Spot ref = graph.vertexRef();
+				System.out.println( ModelUtils.dump(
+						sv.vertices( graph.vertices().getRefPool() ),
+						sv.edges( graph.edges().getRefPool() ),
+						featureModel,
+						ref ) );
+				graph.releaseRef( ref );
+			}
+			else
+			{
+				System.out.println( "Got unexpected result: " + result );
+			}
+		}
+		catch ( final IllegalArgumentException iae )
+		{
+			System.out.println( "Incorrect syntax: " + evaluator.getErrorMessage() );
+			iae.printStackTrace();
+		}
 	}
+
 
 	public static ObjectGraph< String > toMastodon( final SyntaxTree syntaxTree )
 	{
@@ -71,5 +144,24 @@ public class TestParsington
 			graph.addEdge( source, target );
 		}
 		return source;
+	}
+
+	private static ProgressListener voidLogger()
+	{
+		return new ProgressListener()
+		{
+
+			@Override
+			public void showStatus( final String string )
+			{}
+
+			@Override
+			public void showProgress( final int current, final int total )
+			{}
+
+			@Override
+			public void clearStatus()
+			{}
+		};
 	}
 }
