e7716f437bf28a7bd8079fb5aed8df8b0c995064
Jean-Yves Tinevez <tinevez@pasteur.fr>
2022-10-24 10:33:20 +0200

    Update license blurbs.

--- src/main/java/org/mastodon/mamut/selectioncreator/evaluation/FeatureVariable.java
+++ src/main/java/org/mastodon/mamut/selectioncreator/evaluation/FeatureVariable.java
@@ -1,8 +1,8 @@
 /*-
  * #%L
- * mastodon-tracking
+ * mastodon-selection-creator
  * %%
- * Copyright (C) 2018 - 2021 Tobias Pietzsch, Jean-Yves Tinevez
+ * Copyright (C) 2018 - 2022 Tobias Pietzsch, Jean-Yves Tinevez
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
